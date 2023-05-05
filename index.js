a76b22ad0cd19b5f48cd2d4fcd9b8df1661d78af
Riajul Islam <kmriajulislami@gmail.com>
2023-05-05 12:43:45 +0600

    fix: svg-to-ttf-flutter-icons bin support fixed.

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const app = require("./src/app.js");
+const PKG = require('./package.json');
 
-const { program } = require('commander');
+/**
+ * @param {import('commander').Command} program
+ */
+module.exports = function makeProgram(program) {
+    program
+        .name(PKG.name)
+        .description(PKG.description)
+        .version(PKG.version, '-v, --version');
 
-program
-  .name('svg-to-ttf-flutter-icons')
-  .description('The program will ttf files & dart codes, and generate a font class.')
-  .version('1.0.0');
-
-program
-    .command('generate')
-    .description('Generate ttf files & dart codes. You should add the ttf file in pubspec.yaml.')
-    .option('--input', 'The path of the svg files. This should a directory.', './icons')
-    .option('--ttf', 'The output path of the ttf file.', './assets/fonts/AppIcons.ttf')
-    .option('--dart', 'The output path of the dart file. the class name will be according the ttf file name, eg: AppIcons.ttf -> AppIcons', './lib/icons/icons.dart')
-    .action(async (options) => {
-        const {input, ttf, dart} = options;
-        await app(input, ttf, dart);
-    });
-
-program.parse();
+    program
+        .command('generate')
+        .description('Generate ttf files & dart codes. You should add the ttf file in pubspec.yaml.')
+        .option('--input', 'The path of the svg files. This should a directory.', './icons')
+        .option('--ttf', 'The output path of the ttf file.', './assets/fonts/AppIcons.ttf')
+        .option('--dart', 'The output path of the dart file. the class name will be according the ttf file name, eg: AppIcons.ttf -> AppIcons', './lib/icons/icons.dart')
+        .action(async (options) => {
+            const {input, ttf, dart} = options;
+            await app(input, ttf, dart);
+        });
+};
