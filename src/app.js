926e5ca64ebeab1ad389ee2dd59be4fe0558afb5
Riajul Islam <kmriajulislami@gmail.com>
2023-05-05 14:56:27 +0600

    feat: improved.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,27 @@ const copy = require("./copy");
 const fix = require("./fix");
 const config = require("./config");
 const generate = require("./generate");
+const fs = require("fs");
 
 async function app(input, ttf, dart) {
+    if (!fs.existsSync(input)) {
+        throw new Error(`The input path ${input} does not exist.`);
+    }
+
+    let ttfDir = ttf.split('/');
+    ttfDir.pop();
+    ttfDir = ttfDir.join('/');
+    if (!fs.existsSync(ttfDir)) {
+        fs.mkdirSync(ttfDir, {recursive: true});
+    }
+
+    let dartDir = dart.split('/');
+    dartDir.pop();
+    dartDir = dartDir.join('/');
+    if (!fs.existsSync(dartDir)) {
+        fs.mkdirSync(dartDir, {recursive: true});
+    }
+
     copy(input);
     await fix();
     config(ttf);
