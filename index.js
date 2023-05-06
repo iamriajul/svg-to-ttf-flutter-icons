86df67c9c868d563ea6197ca8d98fa1e4b7dc519
Riajul Islam <kmriajulislami@gmail.com>
2023-05-06 22:07:50 +0600

    fix: command option should take string.

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ module.exports = function makeProgram(program) {
     program
         .command('generate')
         .description('Generate ttf files & dart codes. You should add the ttf file in pubspec.yaml.')
-        .option('--input', 'The path of the svg files. This should a directory.', './icons')
-        .option('--ttf', 'The output path of the ttf file.', './assets/fonts/AppIcons.ttf')
-        .option('--dart', 'The output path of the dart file. the class name will be according the ttf file name, eg: AppIcons.ttf -> AppIcons', './lib/icons/icons.dart')
-        .action(async (options) => {
+        .option('--input <string>', 'The path of the svg files. This should a directory.', './icons')
+        .option('--ttf <string>', 'The output path of the ttf file.', './assets/fonts/AppIcons.ttf')
+        .option('--dart <string>', 'The output path of the dart file. the class name will be according the ttf file name, eg: AppIcons.ttf -> AppIcons', './lib/icons/icons.dart')
+        .action(async (options, command) => {
             const {input, ttf, dart} = options;
             await app(input, ttf, dart);
         });
